f7cc649f40ec4f8220234f013ed7368cdda10fd7
Mohamed Khames <mokhames@s1r5p1.1337.ma>
2021-10-09 19:51:53 +0100

    parsing the pipes

---
+++ mini/includes/minishell.h
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mokhames <mokhames@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/03 11:26:17 by mokhames          #+#    #+#             */
+/*   Updated: 2021/10/09 13:17:19 by mokhames         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+typedef struct	s_env
+{
+	char			*value;
+	struct s_env	*next;
+}				t_env;
+typedef struct s_redirect
+{
+        char *file;
+       //char  **argument;
+        int     type;
+}               t_redirect;
+
+typedef struct  s_command
+{
+        char    *cmd;
+		int		t[300];
+        int count;
+        char **argument;
+        t_redirect  *redirect;
+        struct s_command *nextcmd;
+        
+}               t_command;
+
+typedef struct	s_main
+{
+	char	*line;
+    t_env	*env;
+	t_env	*secret_env;
+    int		count;
+    int     t[300];
+    t_command       *cmd;
+
+}				t_main;
+
+
+
+/* ----------------------- libft util --------------------------*/
+void		ft_putstr_fd(char *s, int fd);
+char		**ft_split(const char *str, char c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strnstr(const char *str, const char *to_find, size_t len);
+char		*ft_strjoin(char *s1, char *s2);
+int			ft_strlen(char *c);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strcpy(char *dest, char *src);
+char		*ft_strdup(char *src);
+
+/*-------------------------- list minupilation -----------------*/
+
+t_command	*new_stack(char *a);
+int			ft_lstsize1(t_command *lst);
+void		ft_lstadd_front1(t_command **alst, t_command *new);
+void		ft_lstadd_back1(t_command **alst, t_command *new);
+t_command	*delete_first(t_command *a);
+
+/*------------------------- MINISHELL - parse---------------------------*/
+void    parse(t_main *main);
+int     check_quotes(char c, int open);
+void   parse_redirection(t_command *cmd);
+/*------------------------------ MINISHELL - exec ----------------------*/
+void		error(void);
+char		*find_path(char *cmd, char **envp);
+void		cmd_call(char *argv, char **envm);
+
+/* Bonus functions */
+int			open_file(char *argv, int i);
+void		arg_err(void);
+void		check_cmd(char *inpt, char **cmd, char **envm);
+
+#endif
